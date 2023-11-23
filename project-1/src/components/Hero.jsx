93c06a0156f9915ef91467d6a42f6182e2b36284
huzaifa <syedhuzaifa046@gmail.com>
2023-11-23 16:09:50 +0500

    first commit

---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return <main className="hero">
+    <div className="hero-content">
+    <h1>YOUR FEET DERSERVE THE BEST</h1> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec diam ultricies</p>
+    </div>
+    <div className="hero-image"></div>
+  </main>
+
+};
+
+export default Hero;
---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+const Navigation = () =>{
+  return(
+    <div>
+    <nav>
+      <div className="logo">
+      <img src="/public/brand_logo.png" alt="logo" />
+      </div>
+
+      <ul>
+        <li href="#">Menue</li>
+        <li href="#">Location</li>
+        <li href="#">about</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>login</button>
+    </nav>
+  </div>
+  )
+
+}
+
+export default Navigation;
